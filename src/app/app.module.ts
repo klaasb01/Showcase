6991de9eebd511e14fe442beaf6ead55220f7dbe
unknown <10063026@FLHCL007133W7.BDX.com>
2017-10-20 15:37:23 -0400

    Playing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GaugesComponent } from './gauges/gauges.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GaugesComponent,
+    WelcomeComponent,
+    FileUploadComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'welcome', component: WelcomeComponent },
+      {path: 'gauges', component: GaugesComponent },
+      {path: 'fileupload', component: FileUploadComponent },
+      {path: '', redirectTo: 'gauges', pathMatch: 'full'},
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
